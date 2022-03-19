c9127eaea70d041a6c74cfdfa9d88da5fe34c914
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2022-03-18 21:01:59 -0700

    initial build test
    test build upload

---
+++ hardhat.config.js
@@ -0,0 +1,58 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+require("@nomiclabs/hardhat-etherscan");
+
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      }
+    ],
+  },
+
+  networks: {
+    
+    hardhat: {
+      chainId: 1337
+    },
+
+    rinkeby: {
+      url: process.env.RINKEBY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 300000000,
+      gasPrice: 100000000000,
+      saveDeployments: true,
+    },
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    xdai: {
+      url: process.env.XDAI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
+    },
+    mumbai: {
+      url: process.env.MUMBAI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 200000000,
+      gasPrice: 100000000000,
+      saveDeployments: true,
+    }
+
+ },
+ etherscan: {
+  // Your API key for Etherscan
+  // Obtain one at https://etherscan.io/
+  apiKey: process.env.ETHERSCAN_API_KEY
+}
+};
